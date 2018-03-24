dd6aefa0a71e922e3688ee70e6670459fb5d5ed1
ceschi <emanuele.franceschi@gmail.com>
2018-03-24 21:22:40 +0100

    lol

--- ekn_expl.R
+++ ekn_expl.R
@@ -1,7 +1,10 @@
 ##### Set of explorative regressions #####
 
-
+### for this section most complete DF are better
+### employed, they will possibly extend to other
+### DF if they contain the same variables
 
 
 ##### Plain pooled OLS #####
 
+ols_simple <- lm(data=full_ind, formula = )
