19e9c8710f70e424f0d5dfcb4c1f3860185baa6e
ceschi <emanuele.franceschi@gmail.com>
2018-04-09 18:19:49 +0200

    qcew

--- dirfunc.R
+++ dirfunc.R
@@ -65,7 +65,7 @@ qcew_data_single <- function(data_dir){
 
   # time-consuming loop to download
   # select and merge datasets
-  for (i in 1990:(year(Sys.Date())-1)){
+  for (i in 1990:(year(Sys.Date())-2)){
 
     # compose url with looped year
     url <- paste0(url_core, i, '/csv/', i, url_suff)
@@ -85,100 +85,102 @@ qcew_data_single <- function(data_dir){
 
     # read in tibble the file, select relevant vars
     # and relevant observations
-    db_qcew_temp <- readr::read_csv(file=file.path(data_dir, paste0(i, file_path)),
-                                    col_names = T,
-                                    na=c('','N/A'),
-                                    progress=T,
-                                    quote="\"",
-                                    col_types = c(
-                                      area_fips = col_character(),
-                                      own_code = col_character(),
-                                      industry_code = col_character(),
-                                      agglvl_code = col_character(),
-                                      size_code = col_character(),
-                                      year = col_character(),
-                                      qtr = col_character(),
-                                      disclosure_code = col_character(),
-                                      qtrly_estabs = col_integer(),
-                                      month1_emplvl = col_integer(),
-                                      month2_emplvl = col_integer(),
-                                      month3_emplvl = col_integer(),
-                                      total_qtrly_wages = col_double(),
-                                      taxable_qtrly_wages = col_integer(),
-                                      qtrly_contributions = col_integer(),
-                                      avg_wkly_wage = col_integer(),
-                                      lq_disclosure_code = col_character(),
-                                      lq_qtrly_estabs = col_double(),
-                                      lq_month1_emplvl = col_double(),
-                                      lq_month2_emplvl = col_double(),
-                                      lq_month3_emplvl = col_double(),
-                                      lq_total_qtrly_wages = col_double(),
-                                      lq_taxable_qtrly_wages = col_double(),
-                                      lq_qtrly_contributions = col_double(),
-                                      lq_avg_wkly_wage = col_double(),
-                                      oty_disclosure_code = col_character(),
-                                      oty_qtrly_estabs_chg = col_integer(),
-                                      oty_qtrly_estabs_pct_chg = col_double(),
-                                      oty_month1_emplvl_chg = col_integer(),
-                                      oty_month1_emplvl_pct_chg = col_double(),
-                                      oty_month2_emplvl_chg = col_integer(),
-                                      oty_month2_emplvl_pct_chg = col_double(),
-                                      oty_month3_emplvl_chg = col_integer(),
-                                      oty_month3_emplvl_pct_chg = col_double(),
-                                      oty_total_qtrly_wages_chg = col_integer(),
-                                      oty_total_qtrly_wages_pct_chg = col_double(),
-                                      oty_taxable_qtrly_wages_chg = col_integer(),
-                                      oty_taxable_qtrly_wages_pct_chg = col_double(),
-                                      oty_qtrly_contributions_chg = col_integer(),
-                                      oty_qtrly_contributions_pct_chg = col_double(),
-                                      oty_avg_wkly_wage_chg = col_integer(),
-                                      oty_avg_wkly_wage_pct_chg = col_double()
-                                    )
-                                    ) %>%
-      select(area_fips,
-             own_code,
-             industry_code,
-             size_code,
-             year,
-             qtr,
-             qtrly_estabs,
-             month3_emplvl,
-             total_qtrly_wages,
-             avg_wkly_wage,
-             oty_qtrly_estabs_chg,
-             oty_qtrly_estabs_pct_chg,
-             oty_total_qtrly_wages_chg,
-             oty_total_qtrly_wages_pct_chg,
-             oty_avg_wkly_wage_chg,
-             oty_avg_wkly_wage_pct_chg,
-             oty_month1_emplvl_chg,
-             oty_month2_emplvl_chg,
-             oty_month3_emplvl_chg,
-             oty_month1_emplvl_pct_chg,
-             oty_month2_emplvl_pct_chg,
-             oty_month3_emplvl_pct_chg
-      )
-      #        ) %>%
-      # 
-      # # filter only aggregate and private observations
-      # filter(own_code %in% c(0,5)) %>%
-      # # filter only US-wide observations
-      # filter(area_fips=='US000')
-
-    ### WORK THIS OUT!! ###
-    out_db <- bind_rows(out_db, db_qcew_temp)
-
-    # problem with this data is that we loose entirely the
-    # size dimension of the observations.
-    # Data by industry might provide more suitability.
-    
-    
-    # remove csv file
-    file.remove(file.path(data_dir, paste0(i, file_path)))
+    # db_qcew_temp <- read.csv(file=file.path(data_dir, paste0(i, file_path)),
+    #                                 header = T,
+    #                                 na.strings=c('','N/A'),
+    #                                 dec='.',
+    #                                 sep=',',
+    #                                 stringsAsFactors = F,
+    #                                 quote="\""#,
+    #                                 # col_types = c(
+    #                                 #   area_fips = col_character(),
+    #                                 #   own_code = col_character(),
+    #                                 #   industry_code = col_character(),
+    #                                 #   agglvl_code = col_character(),
+    #                                 #   size_code = col_character(),
+    #                                 #   year = col_character(),
+    #                                 #   qtr = col_character(),
+    #                                 #   disclosure_code = col_character(),
+    #                                 #   qtrly_estabs = col_integer(),
+    #                                 #   month1_emplvl = col_integer(),
+    #                                 #   month2_emplvl = col_integer(),
+    #                                 #   month3_emplvl = col_integer(),
+    #                                 #   total_qtrly_wages = col_double(),
+    #                                 #   taxable_qtrly_wages = col_integer(),
+    #                                 #   qtrly_contributions = col_integer(),
+    #                                 #   avg_wkly_wage = col_integer(),
+    #                                 #   lq_disclosure_code = col_character(),
+    #                                 #   lq_qtrly_estabs = col_double(),
+    #                                 #   lq_month1_emplvl = col_double(),
+    #                                 #   lq_month2_emplvl = col_double(),
+    #                                 #   lq_month3_emplvl = col_double(),
+    #                                 #   lq_total_qtrly_wages = col_double(),
+    #                                 #   lq_taxable_qtrly_wages = col_double(),
+    #                                 #   lq_qtrly_contributions = col_double(),
+    #                                 #   lq_avg_wkly_wage = col_double(),
+    #                                 #   oty_disclosure_code = col_character(),
+    #                                 #   oty_qtrly_estabs_chg = col_integer(),
+    #                                 #   oty_qtrly_estabs_pct_chg = col_double(),
+    #                                 #   oty_month1_emplvl_chg = col_integer(),
+    #                                 #   oty_month1_emplvl_pct_chg = col_double(),
+    #                                 #   oty_month2_emplvl_chg = col_integer(),
+    #                                 #   oty_month2_emplvl_pct_chg = col_double(),
+    #                                 #   oty_month3_emplvl_chg = col_integer(),
+    #                                 #   oty_month3_emplvl_pct_chg = col_double(),
+    #                                 #   oty_total_qtrly_wages_chg = col_character(),
+    #                                 #   oty_total_qtrly_wages_pct_chg = col_double(),
+    #                                 #   oty_taxable_qtrly_wages_chg = col_double(),
+    #                                 #   oty_taxable_qtrly_wages_pct_chg = col_double(),
+    #                                 #   oty_qtrly_contributions_chg = col_integer(),
+    #                                 #   oty_qtrly_contributions_pct_chg = col_double(),
+    #                                 #   oty_avg_wkly_wage_chg = col_integer(),
+    #                                 #   oty_avg_wkly_wage_pct_chg = col_double()
+    #                                 # )
+    #                                 ) %>%
+    #   select(area_fips,
+    #          own_code,
+    #          industry_code,
+    #          size_code,
+    #          year,
+    #          qtr,
+    #          qtrly_estabs,
+    #          month3_emplvl,
+    #          total_qtrly_wages,
+    #          avg_wkly_wage,
+    #          oty_qtrly_estabs_chg,
+    #          oty_qtrly_estabs_pct_chg,
+    #          oty_total_qtrly_wages_chg,
+    #          oty_total_qtrly_wages_pct_chg,
+    #          oty_avg_wkly_wage_chg,
+    #          oty_avg_wkly_wage_pct_chg,
+    #          oty_month1_emplvl_chg,
+    #          oty_month2_emplvl_chg,
+    #          oty_month3_emplvl_chg,
+    #          oty_month1_emplvl_pct_chg,
+    #          oty_month2_emplvl_pct_chg,
+    #          oty_month3_emplvl_pct_chg
+    #   )
+    #   #        ) %>%
+    #   # 
+    #   # # filter only aggregate and private observations
+    #   # filter(own_code %in% c(0,5)) %>%
+    #   # # filter only US-wide observations
+    #   # filter(area_fips=='US000')
+    # 
+    # ### WORK THIS OUT!! ###
+    # out_db <- bind_rows(out_db, db_qcew_temp)
+    # 
+    # # problem with this data is that we loose entirely the
+    # # size dimension of the observations.
+    # # Data by industry might provide more suitability.
+    # 
+    # 
+    # # remove csv file
+    # file.remove(file.path(data_dir, paste0(i, file_path)))
   }
 
-  return(out_db)
-  write.table(out_db, file = file.path(data.dir, 'datamonster.csv'), sep = ',')
+  # return(out_db)
+  # write.table(out_db, file = file.path(data.dir, 'datamonster.csv'), sep = ',')
 }
 
 
